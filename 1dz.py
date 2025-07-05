c5f3e24c4924204e134c28786ac429bd10be237d
MAZAXUS <m.eldarov99@gmail.com>
2025-07-05 18:41:47 +0300

    [upd] readme

---
+++ 1dz.py
@@ -0,0 +1,36 @@
+# Арифметические операции
+a = 10
+b = 2
+
+sum_result = a + b
+diff_result = a - b
+mult_result = a * b
+div_result = a / b
+
+print(sum_result)
+print(diff_result)
+print(mult_result)
+print(div_result)
+
+
+# Типы данных
+
+num = 15
+text = "Python"
+is_active = False
+
+print(type(num))
+print(type(text))
+print(type(is_active))
+
+
+# Преобразование типов данных
+
+number_str = "123"
+float_num = 45.8
+
+number_int = int(number_str)
+print("Строку в число", number_int)
+
+float_str = str(float_num)
+print(f'"{float_str}"')
